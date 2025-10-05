0b5b245cc1456d9704f07472f954861850bebadf
AresRed <ares19951208@gmail.com>
2025-10-05 00:52:52 -0500

    first commit

---
+++ src/main/java/com/agro/control_asistencia_backend/ControlAsistenciaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agro.control_asistencia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlAsistenciaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlAsistenciaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agro/control_asistencia_backend/ControlAsistenciaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agro.control_asistencia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlAsistenciaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
